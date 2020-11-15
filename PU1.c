677eb3e9b3bacdaa64bb9a11d1f3d2639dae1ea9
MikusRi <71380628+MikusRi@users.noreply.github.com>
2020-11-15 21:22:12 +0200

    Add files via upload

---
+++ PU1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main ()
+ {
+	float a, b, c;
+	printf("Ievadiet pirmo skaitli:\n");
+	scanf("%f",&a);
+	printf("Ievadiet otru skaitli:\n");
+	scanf("%f",&b);
+	c = a * b;
+	printf("Rezultāts: %.f\n",c);
+
+return 0;
+}
